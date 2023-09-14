24240a81d6c20260d0381bcbd8ac38f04dd76f65
Eduardo Jesus <126268026+EdwJezus@users.noreply.github.com>
2023-09-14 17:58:38 -0300

    Update client.py

--- client.py
+++ client.py
@@ -6,8 +6,12 @@ def receive_messages(client):
         msg = client.recv(1024).decode('utf-8')
         print(msg)
 
+server_ip = input('Digite o endereço IP do servidor: ')##PARA SER ALÉM DO LOCAL
+server_port = 8888  # Porta do servidor, PARA SER ALÉM DO LOCAL
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('localhost', 8888))
+##client.connect(('localhost', 8888))
+client.connect((server_ip, server_port))
 
 terminado = False
 print('====== CHAT TERMINAL ======')
