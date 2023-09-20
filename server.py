ce9d36888e204d4d64e0bb4226db948eea0bcae2
Eduardo Jesus <126268026+EdwJezus@users.noreply.github.com>
2023-09-20 02:10:22 -0300

    Update server.py
    adicionado o comando @ordenar

--- server.py
+++ server.py
@@ -1,16 +1,28 @@
 import socket
 import threading
+from datetime import datetime
+
+mensagens = []
 
 def handle_client(client):
-    client_address = client.getpeername()  #Obtém o endereço do cliente
+    client_address = client.getpeername()
     while True:
         msg = client.recv(1024).decode('utf-8')
-        if msg == '@SAIR':
+        if msg.upper() == '@SAIR':
             clients.remove(client)
             client.close()
             print(f'Conexão encerrada por cliente {client_address}.')
             break
+        elif msg.upper() == '@ORDENAR':
+            ordem_mensagens = sorted(mensagens, key=lambda x: x[1])
+            response = ""
+            for mensagem, hora, sender_address in ordem_mensagens[-15:]:
+                response += f'{sender_address} {hora}:: {mensagem}\n'
+            client.send(response.encode('utf-8'))
         else:
+            hora = datetime.now().strftime('%H:%M:%S')
+            ##timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            mensagens.append((msg, hora, client_address))
             print(f'{client_address}: {msg}')
             for other_client in clients:
                 if other_client != client:
@@ -23,16 +35,15 @@ def handle_client(client):
                         break
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-##server.bind(('0.0.0.0', 8888))
-server_ip = '0.0.0.0'  #ou endereço IP da máquina onde o servidor ta sendo executado
-server_port = 8888     #porta do servidor
+server_ip = '0.0.0.0'
+server_port = 8888
 server.bind((server_ip, server_port))
 server.listen()
 
 clients = []
 
 print('====== CHAT TERMINAL ======')
-print('='*30)
+print('=' * 30)
 print('Servidor esperando por conexões...')
 
 while True:
@@ -44,6 +55,5 @@ while True:
     client_thread.start()
 
 #IMPLEMENTAR
-####@ORDENAR: mostra as últimas 15 mensagens, ordenadas pelo horário de envio
 ####@UPLOAD : faz upload de um arquivo para o servidor
 ####@DOWNLOAD : faz download de um arquivo do servidor
